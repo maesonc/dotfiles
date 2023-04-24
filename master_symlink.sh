40568d88b8ea8d35422fb0261a7e9dfe64d0b0bb
Maeson Chan <maeson.chan@gmail.com>
2023-04-24 10:44:40 +0800

    Updated starship config to symlink

--- master_symlink.sh
+++ master_symlink.sh
@@ -8,5 +8,6 @@ ln -s -f $HOME/git-personal/MAESON/dotfiles/alacritty/alacritty.yml $HOME/.confi
 ln -s -f $HOME/git-personal/MAESON/dotfiles/fish/config.fish $HOME/.config/fish/config.fish
 ln -s -f $HOME/git-personal/MAESON/dotfiles/fish/omf.fish $HOME/.config/fish/conf.d/omf.fish
 ln -s -f $HOME/git-personal/MAESON/dotfiles/fish/fish_greeting.fish $HOME/.config/fish/functions/fish_greeting.fish
+ln -s -f $HOME/git-personal/MAESON/dotfiles/fish/starship.toml $HOME/.config/starship.toml
 
 echo "Symlinks Completed";
