ec933d6046b1a80d6d3cebd916b89d2c341f53ce
Maeson Chan <maeson.chan@gmail.com>
2023-04-24 10:42:23 +0800

    Updated to work for Mac

--- main.sh
+++ main.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-packagesNeeded='git npm neovim fish curl lolcat cowsay php nodejs tmux alacritty'
+packagesNeeded='git npm neovim fish curl lolcat cowsay php nodejs tmux alacritty fortune';
+
+is_mac=0;
 
 if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded 
 elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
 elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
-elif [ -x "$(command -v brew)" ];  then brew install $packagesNeeded
+elif [ -x "$(command -v brew)" ];  then brew install $packagesNeeded; $is_mac=1;
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; $proceed_vim_setup=0; fi
 
 mkdir -p $HOME/.config/alacritty
@@ -37,3 +39,16 @@ else
 		exit "Could not change default shell.";
 	fi
 fi
+
+if [ $ismac == 1 ];
+then
+	echo "Installing hack font on Mac";
+	brew tap homebrew/cask-fonts && brew install font-hack-nerd-font
+else
+	echo "Installing hack font on Linux";
+	cd $HOME/git-personal/MAESON/  && git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts;
+	cd $HOME/git-personal/MAESON/nerd-fonts;
+	git sparse-checkout add patched-fonts/Hack;
+	source install.sh Hack;
+fi
+
--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 
 proceed_vim_setup=1;
  #Install vim vundle.
@@ -6,9 +6,9 @@ git --version 2>&1 >/dev/null # improvement by tripleee
 GIT_IS_AVAILABLE=$?
 if [ $GIT_IS_AVAILABLE != 0 ];
 then
-	read -p "'git' command not found. Do you want to install Git? [y/N]" response 
+	read -p "'git' command not found. Do you want to install Git? [y/N]" response
 
-	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+	if [[ "$response" =~ "^([yY][eE][sS]|[yY])$" ]];
 	then
 		packagesNeeded='git npm neovim'
 		if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded 
@@ -16,14 +16,14 @@ then
 		elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 		elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 		elif [ -x "$(command -v brew)" ];  then brew install $packagesNeeded
-		else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; $proceed_vim_setup=0; fi
+		else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; $proceed_vim_setup=0;fi
 	else
 		echo "Please manually install $packagesNeeded before proceeding.";
 		exit 0
 	fi
 fi
 
-if [ $proceed_vim_setup == 1 ];
+if [ $proceed_vim_setup = 1 ];
 then
 	echo "Setting up vim";
 
@@ -38,20 +38,20 @@ then
 	cd $HOME/.config/nvim/bundle/coc.nvim
 	npm install
 
-	nvim "CocInstall coc-tsserver" +qall +silent
-	nvim "CocInstall coc-tslint" +qall +silent
-	nvim "CocInstall coc-tailwindcss" +qall +silent
-	nvim "CocInstall coc-sqlfluff" +qall +silent
-	nvim "CocInstall coc-sql" +qall +silent
-	nvim "CocInstall coc-snippets" +qall +silent
-	nvim "CocInstall coc-smartf" +qall +silent
-	nvim "CocInstall coc-pyright" +qall +silent
-	nvim "CocInstall coc-phpactor" +qall +silent
-	nvim "CocInstall coc-omnisharp" +qall +silent
-	nvim "CocInstall coc-json" +qall +silent
-	nvim "CocInstall coc-html" +qall +silent
-	nvim "CocInstall coc-fzf-preview" +qall +silent
-	nvim "CocInstall coc-css" +qall +silent
+	nvim +"CocInstall -sync coc-tsserver" +qall
+	nvim +"CocInstall -sync coc-tslint" +qall
+	nvim +"CocInstall -sync coc-tailwindcss" +qall
+	nvim +"CocInstall -sync coc-sqlfluff" +qall
+	nvim +"CocInstall -sync coc-sql" +qall
+	nvim +"CocInstall -sync coc-snippets" +qall
+	nvim +"CocInstall -sync coc-smartf" +qall
+	nvim +"CocInstall -sync coc-pyright" +qall
+	nvim +"CocInstall -sync coc-phpactor" +qall
+	nvim +"CocInstall -sync coc-omnisharp" +qall
+	nvim +"CocInstall -sync coc-json" +qall
+	nvim +"CocInstall -sync coc-html" +qall
+	nvim +"CocInstall -sync coc-fzf-preview" +qall
+	nvim +"CocInstall -sync coc-css" +qall
 
 	echo "Nvim is ready to go!"
 fi
